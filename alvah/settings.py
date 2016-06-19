0db3808da97adf604c4a10cef9f2db53bc2b363a
Brandon Conway <brandoncowboy@gmail.com>
2016-06-19 01:09:13 +0000

    Changes to settings and wsgi file

--- alvah/settings.py
+++ alvah/settings.py
@@ -125,6 +125,7 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 SITE_ID = 1
 
+SITE_TITLE = 'Stories by Alvah'
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
 USE_I18N = False
--- alvah/wsgi.py
+++ alvah/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/storiesbyalvah.com/htdocs'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                      "%s.settings" % real_project_name("alvah"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alvah.settings")
 
 application = get_wsgi_application()
